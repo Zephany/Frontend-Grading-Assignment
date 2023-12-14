89dff432ef54c05f11184361505ceb68d801fc60
Zephany <zephanilaopoku4@gmail.com>
2023-12-14 21:12:35 +0000

    footer page

---
+++ my-app/src/Components/footer/footer.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import './footer.css'
+
+function    Footer ()
+{
+    return(
+        <div>
+             <footer class="footer">
+    <div class="footer-rw-1">
+        <div class="footer-rw-col">
+            <div class="col-tile">
+                <h3>Courses</h3>
+            </div>
+            <div class="col-links-container">
+                <a class="col-link">DCIT 201(Programming)</a>
+                <a class="col-link">DCIT 203(Digital and Logic System Design)</a>
+                <a class="col-link">DCIT 205(Multimedia Web Design)</a>
+                <a class="col-link">DCIT 207(Computer Organization and Architecture)</a>
+                <a class="col-link">DCIT 209(E-Business Architecture)</a>
+                <a class="col-link">CBAS(Academic Writting II)</a>
+            </div>
+        </div>
+        <div class="footer-rw-col">
+            <div class="col-tile">
+                <h3>FAQ</h3>
+            </div>
+            <div class="col-links-container">
+                <a class="col-link">Undergraduate</a>
+                <a class="col-link">Graduate</a>
+                <a class="col-link">International Students</a>
+                <a class="col-link">Entry Requirements</a>
+                <a class="col-link">Contact Us</a>
+            </div>
+        </div>
+        <div class="footer-rw-col">
+            <div class="col-tile">
+                <h3>Research</h3>
+            </div>
+            <div class="col-links-container">
+                <a class="col-link">Research at DCS</a>
+                <a class="col-link">Research Impact</a>
+                <a class="col-link">Libraries</a>
+            </div>
+        </div>
+        <div class="footer-rw-col">
+            <div class="col-tile">
+                <h3>About the Department</h3>
+            </div>
+            <div class="col-links-container">
+                <a class="col-link">Introducing DCS</a>
+                <a class="col-link">Head of Department's Office</a>
+                <a class="col-link">Faculty</a>
+                <a class="col-link">Events</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="footer-rw-1 copy">
+        <p>Department of Computer Science</p>
+        <p>Copyright &copy;2023</p>
+        <p>All rights Reserved</p>
+    </div>
+</footer>
+             
+              
+        </div>
+
+    )
+}
+
+
+
+
+export default Footer
